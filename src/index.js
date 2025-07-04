7ec40aed83ac7c0d24e95496a8cf6d873e8253df
gldsct <188839581+gldsct@users.noreply.github.com>
2025-07-04 20:38:41 +0545

    Implement dynamic weather data display
    Implement functionality to fetch and display weather data for the user
    specified location. The data is displayed in a separate divbox container
    centrally placed in the webpage. This commit also styles and lays out
    content within the divbox container.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./styles.css"
 import { apiKey } from "./api_key";
+import locationIconImage from "./images/location.png";
+import morningImage from "./images/morning.jpg";
+import nightImage from "./images/night.jpg";
 
 async function getApiResponse (location) {
     try {
@@ -17,61 +20,165 @@ function getDaysFeatures (address, day) {
     const time = day.datetime;
     const conditions = day.conditions;
     const temp = day.temp;
-    const tempFeelsLike = day.feelslike;
     const humidity = day.humidity;
     const windSpeed = day.windspeed;
     const icon = day.icon;
-    console.log(`In ${address}, on ${time}: Temperature = ${temp}°C (feels like ${tempFeelsLike}°C), Humidity = ${humidity}%, Wind = ${windSpeed} kmph.`);
-    console.log(`Condition: ${conditions}. Icon: ${icon}`);
+    const weatherData = {
+        address,
+        time,
+        conditions,
+        temp,
+        humidity,
+        windSpeed,
+        icon
+    };
+    return weatherData;
 }
 
 async function getData (location, date, day) {
     const apiResponse = await getApiResponse(location);
-    console.log(apiResponse);
+    let features;
     if (arguments.length === 2) {
         switch (date) {
             case "current":
-                getDaysFeatures(apiResponse.resolvedAddress, apiResponse["currentConditions"]);
+                features = await getDaysFeatures(apiResponse.resolvedAddress, apiResponse["currentConditions"]);
                 break;
             case "today":
-                getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][0]);
+                features = await getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][0]);
                 break;
         }
     }
     else {
         switch (day) {
             case 1:
-                getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][1]);
+                features = await getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][1]);
                 break;
             case 2:
-                getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][2]);
+                features = await getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][2]);
                 break;
             case 3:
-                getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][3]);
+                features = await getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][3]);
                 break;
             case 4:
-                getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][4]);
+                features = await getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][4]);
                 break;
             case 5:
-                getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][5]);
+                features = await getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][5]);
                 break;
             case 6:
-                getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][6]);
+                features = await getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][6]);
                 break;
             case 7:
-                getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][7]);
+                features = await getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][7]);
                 break;
             default:
                 console.log("Invalid date.");
             }
     }
+    return features;
 }
 
 const inputLocation = document.querySelector("#location");
 const submitButton = document.querySelector(".location-input-submit");
+const mainContent = document.querySelector(".main-weather-content");
+const miscContent = document.querySelector(".misc-weather-content");
 
 submitButton.addEventListener("click", (event) => {
     event.preventDefault();
-    getData(inputLocation.value, "current");
+    getData(inputLocation.value, "current").then((resolve) => {
+        console.log(resolve);
+        showWeather(resolve);
+    });
     inputLocation.value = "";
 });
+
+function showWeather (features) {
+    mainContent.textContent = "";
+    miscContent.textContent = "";
+
+    function showMainContent () {
+        (function showBackgroundImage () {
+            const weatherCard = document.querySelector(".weather-card");
+            if (features.time > "12:00:00" && features.time < "23:59:59") {
+                weatherCard.style.backgroundImage = `url(${nightImage})`;
+            }
+            else {
+                weatherCard.style.backgroundImage = `url(${morningImage})`;
+            }
+        })();
+
+        (function showLocationInfo () {
+            const location = document.createElement("div");
+            location.classList.add("weather-location");
+    
+            const locationIcon = document.createElement("img");
+            locationIcon.src = locationIconImage;
+            locationIcon.width = 25;
+            locationIcon.height = 25;
+            location.appendChild(locationIcon);
+    
+            const locationName = document.createElement("p");
+            locationName.textContent = features.address;
+            location.appendChild(locationName);
+            mainContent.appendChild(location);
+
+            const time = document.createElement("p");
+            time.classList.add("location-time");
+            time.textContent = features.time;
+            mainContent.appendChild(time);
+        })();
+
+        (function showStats () {
+            (function showMainStats () {
+                const mainStats = document.createElement("div");
+                mainStats.classList.add("location-main-stats");
+    
+                const temperature = document.createElement("p");
+                temperature.classList.add("location-temperature");
+                temperature.textContent = `${features.temp}°`;
+    
+                const status = document.createElement("div");
+                const statusText = document.createElement("p");
+                statusText.textContent = features.conditions;
+                
+                const statusImage = document.createElement("img");
+                import(`./images/WeatherIcons/${features.icon}.svg`)
+                .then((module) => {
+                    statusImage.src = module.default;
+                })
+                .catch(() => {
+                    throw Error ("Could not load icon.");
+                });
+                statusImage.classList.add("location-status-image");
+    
+                status.appendChild(statusImage);
+                status.appendChild(statusText);
+    
+                mainStats.appendChild(status);
+                mainStats.appendChild(temperature);
+    
+                mainContent.appendChild(mainStats);
+            })();
+
+            (function showMiscStats () {
+                const miscStats = document.createElement("div");
+                miscStats.classList.add("location-misc-stats");
+
+                const humidity = document.createElement("p");
+                humidity.textContent = "Humidity - " + features.humidity + "%";
+
+                const windspeed = document.createElement("p");
+                windspeed.textContent = "Wind - " + features.windSpeed + " km/h";
+
+                miscStats.appendChild(humidity);
+                miscStats.appendChild(windspeed);
+
+                mainContent.appendChild(miscStats);
+            })();
+        })();
+
+        mainContent.style.color = (features.time > "12:00:00") ? "white" : "#2C2128";
+    }
+
+    showMainContent();
+}
