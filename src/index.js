b39e5b43150bdc0e5f731f93b90847c4d7357653
gldsct <188839581+gldsct@users.noreply.github.com>
2025-07-07 16:43:36 +0545

    Display hourly weather forecast
    Add hourly weather forecasts to the weather display container.

--- src/index.js
+++ src/index.js
@@ -16,23 +16,34 @@ async function getApiResponse (location) {
     }
 }
 
-function getDaysFeatures (address, day) {
-    const time = day.datetime;
-    const conditions = day.conditions;
-    const temp = day.temp;
-    const humidity = day.humidity;
-    const windSpeed = day.windspeed;
-    const icon = day.icon;
-    const weatherData = {
-        address,
-        time,
-        conditions,
-        temp,
-        humidity,
-        windSpeed,
-        icon
+function getDaysFeatures (address, data) {
+    const currentFeatures = function () {
+        const current = data["currentConditions"];
+        const time = current.datetime;
+        const conditions = current.conditions;
+        const temp = current.temp;
+        const humidity = current.humidity;
+        const windSpeed = current.windspeed;
+        const icon = current.icon;
+        return {
+            address,
+            time,
+            conditions,
+            temp,
+            humidity,
+            windSpeed,
+            icon
+        };
     };
-    return weatherData;
+
+    const hourlyFeatures = function () {
+        const hourly = data["days"][0]["hours"];
+        return hourly;
+    };
+    
+    return { current: currentFeatures(),
+        hourly: hourlyFeatures()
+     };
 }
 
 async function getData (location, date, day) {
@@ -40,36 +51,33 @@ async function getData (location, date, day) {
     let features;
     if (arguments.length === 2) {
         switch (date) {
-            case "current":
-                features = await getDaysFeatures(apiResponse.resolvedAddress, apiResponse["currentConditions"]);
-                break;
             case "today":
-                features = await getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][0]);
+                features = await getDaysFeatures(apiResponse.resolvedAddress, apiResponse);
                 break;
         }
     }
     else {
         switch (day) {
             case 1:
-                features = await getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][1]);
+                features = await getHourlyFeatures(apiResponse.resolvedAddress, apiResponse["days"][1]);
                 break;
             case 2:
-                features = await getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][2]);
+                features = await getHourlyFeatures(apiResponse.resolvedAddress, apiResponse["days"][2]);
                 break;
             case 3:
-                features = await getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][3]);
+                features = await getHourlyFeatures(apiResponse.resolvedAddress, apiResponse["days"][3]);
                 break;
             case 4:
-                features = await getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][4]);
+                features = await getHourlyFeatures(apiResponse.resolvedAddress, apiResponse["days"][4]);
                 break;
             case 5:
-                features = await getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][5]);
+                features = await getHourlyFeatures(apiResponse.resolvedAddress, apiResponse["days"][5]);
                 break;
             case 6:
-                features = await getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][6]);
+                features = await getHourlyFeatures(apiResponse.resolvedAddress, apiResponse["days"][6]);
                 break;
             case 7:
-                features = await getDaysFeatures(apiResponse.resolvedAddress, apiResponse["days"][7]);
+                features = await getHourlyFeatures(apiResponse.resolvedAddress, apiResponse["days"][7]);
                 break;
             default:
                 console.log("Invalid date.");
@@ -85,21 +93,21 @@ const miscContent = document.querySelector(".misc-weather-content");
 
 submitButton.addEventListener("click", (event) => {
     event.preventDefault();
-    getData(inputLocation.value, "current").then((resolve) => {
-        console.log(resolve);
-        showWeather(resolve);
+    getData(inputLocation.value, "today").then((resolve) => {
+        console.log(resolve.current, resolve.hourly);
+        showWeather(resolve.current, resolve.hourly);
     });
     inputLocation.value = "";
 });
 
-function showWeather (features) {
+function showWeather (currentFeatures, hourlyFeatures) {
     mainContent.textContent = "";
     miscContent.textContent = "";
 
     function showMainContent () {
         (function showBackgroundImage () {
             const weatherCard = document.querySelector(".weather-card");
-            if (features.time > "12:00:00" && features.time < "23:59:59") {
+            if (currentFeatures.time > "12:00:00" && currentFeatures.time < "23:59:59") {
                 weatherCard.style.backgroundImage = `url(${nightImage})`;
             }
             else {
@@ -118,13 +126,13 @@ function showWeather (features) {
             location.appendChild(locationIcon);
     
             const locationName = document.createElement("p");
-            locationName.textContent = features.address;
+            locationName.textContent = currentFeatures.address;
             location.appendChild(locationName);
             mainContent.appendChild(location);
 
             const time = document.createElement("p");
             time.classList.add("location-time");
-            time.textContent = features.time;
+            time.textContent = currentFeatures.time;
             mainContent.appendChild(time);
         })();
 
@@ -135,29 +143,27 @@ function showWeather (features) {
     
                 const temperature = document.createElement("p");
                 temperature.classList.add("location-temperature");
-                temperature.textContent = `${features.temp}°`;
-    
-                const status = document.createElement("div");
-                const statusText = document.createElement("p");
-                statusText.textContent = features.conditions;
+                temperature.textContent = `${currentFeatures.temp}°`;
                 
-                const statusImage = document.createElement("img");
-                import(`./images/WeatherIcons/${features.icon}.svg`)
+                const icon = document.createElement("img");
+                import(`./images/WeatherIcons/${currentFeatures.icon}.svg`)
                 .then((module) => {
-                    statusImage.src = module.default;
+                    icon.src = module.default;
                 })
                 .catch(() => {
                     throw Error ("Could not load icon.");
                 });
-                statusImage.classList.add("location-status-image");
+                icon.classList.add("location-status-image");
     
-                status.appendChild(statusImage);
-                status.appendChild(statusText);
-    
-                mainStats.appendChild(status);
+                mainStats.appendChild(icon);
                 mainStats.appendChild(temperature);
+
+                const mainStatsSummary = document.createElement("p");
+                mainStatsSummary.textContent = currentFeatures.conditions;
+                mainStatsSummary.classList.add("location-main-stats-summary");
     
                 mainContent.appendChild(mainStats);
+                mainContent.appendChild(mainStatsSummary);
             })();
 
             (function showMiscStats () {
@@ -165,10 +171,10 @@ function showWeather (features) {
                 miscStats.classList.add("location-misc-stats");
 
                 const humidity = document.createElement("p");
-                humidity.textContent = "Humidity - " + features.humidity + "%";
+                humidity.textContent = "Humidity - " + currentFeatures.humidity + "%";
 
                 const windspeed = document.createElement("p");
-                windspeed.textContent = "Wind - " + features.windSpeed + " km/h";
+                windspeed.textContent = "Wind - " + currentFeatures.windSpeed + " km/h";
 
                 miscStats.appendChild(humidity);
                 miscStats.appendChild(windspeed);
@@ -177,8 +183,74 @@ function showWeather (features) {
             })();
         })();
 
-        mainContent.style.color = (features.time > "12:00:00") ? "white" : "#2C2128";
+        mainContent.style.color = (currentFeatures.time > "12:00:00") ? "white" : "#2C2128";
+    }
+
+    function showMiscContent() {
+        let startIndex = 0;
+        const miscHeader = document.createElement("p");
+        const miscContainer = document.createElement("div");
+
+        let allHourlyFeatures = [];
+        hourlyFeatures.forEach((hour) => {
+            const icon = hour.icon;
+            const temp = hour.temp;
+            const time = hour.datetime.slice(0, 5);
+            const formattedTime = time >= "12:00" ? time.slice(0, 2) === "12" ? `${time} PM` : `${time.slice(0, 2) - 12}:${time.slice(3, 5)} PM` : `${time} AM`; 
+            allHourlyFeatures.push({ formattedTime, temp, icon });
+        });
+        console.log(allHourlyFeatures);
+
+        function showMiscHeader () {
+            miscHeader.classList.add("misc-weather-content-header");
+            miscHeader.textContent = "Weather Today";
+            miscContent.appendChild(miscHeader);
+        };
+
+        function showMiscContainer () {
+            miscContainer.classList.add("misc-weather-content-container");
+
+            startIndex = currentFeatures.time > "20:00:00" ? 20 : Number(currentFeatures.time.slice(0, 2));
+            showDetails(allHourlyFeatures, startIndex);
+
+            miscContent.appendChild(miscContainer);
+        };
+
+        function showDetails (allHourlyFeatures, start) {
+            miscContainer.textContent = "";
+            for (let i = start; i <= start + 3; i++) {
+                const subcontainer = document.createElement("div");
+    
+                const icon = document.createElement("img");
+                import(`./images/WeatherIcons/${allHourlyFeatures[i].icon}.svg`)
+                .then((module) => {
+                    icon.src = module.default;
+                })
+                .catch(() => {
+                    throw Error ("Could not load icon.");
+                });
+                icon.classList.add("misc-hourly-status-icon");
+                subcontainer.appendChild(icon);
+    
+                const time = document.createElement("p");
+                time.classList.add("misc-hourly-time");
+                time.textContent = allHourlyFeatures[i]["formattedTime"];
+                subcontainer.appendChild(time);
+    
+                const temp = document.createElement("p");
+                temp.classList.add("misc-hourly-temp");
+                temp.textContent = allHourlyFeatures[i].temp + "°";
+                subcontainer.appendChild(temp);
+    
+                miscContainer.appendChild(subcontainer);
+            }
+        };
+
+        showMiscHeader();
+        showMiscContainer();
     }
 
     showMainContent();
+    showMiscContent();
+
 }
